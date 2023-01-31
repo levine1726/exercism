30bdb5fbf42e24c9508e12643144d313d97e1e46
David Levine <levine1726@protonmail.com>
2023-01-31 14:54:51 -0500

    apply raindrops feedback

--- bash/raindrops/raindrops.sh
+++ bash/raindrops/raindrops.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
 main () {
-  let number="$1"
+  number=$1
   response=""
   
-  if [[ $(expr $number % 3) == "0" ]]; then
+  if [[ $((number % 3)) == "0" ]]; then
     response="${response}Pling"
   fi
   
-  if [[ $(expr $number % 5) == "0" ]]; then
+  if [[ $((number % 5)) == "0" ]]; then
     response="${response}Plang"
   fi
 
-  if [[ $(expr $number % 7) == 0  ]]; then
+  if [[ $((number % 7)) == 0  ]]; then
     response="${response}Plong"
   fi
 
